c34121586bc5b3e1a93e27bc2a2179ea9be76132
Slimecaster <nd070503@gmail.com>
2024-10-21 15:46:19 +0200

    hi

--- src/app.js
+++ src/app.js
@@ -1,31 +1,21 @@
+
     const express = require('express');
-const path = require('path');
-const app = express();
-const port = 3000;
+    const path = require('path');
+    const logger = require('./middleware/logger');
+    const homeRoutes = require('./routes/homeRoutes');
+    const submitRoutes = require('./routes/submitRoutes');
+    const app = express();
 
-// Middleware til at parse JSON-data
-app.use(express.json());
 
-// Middleware til at logge anmodninger
-app.use((req, res, next) => {
-    console.log(`${req.method} request for '${req.url}'`);
-    next();
-});
+    // Middleware
+    app.use(logger);
+    app.use(express.static(path.join(__dirname, '..', 'public')));
+    app.use(express.json());
 
-// Middleware til at servere statiske filer
-app.use(express.static(path.join(__dirname, 'public')));
+    // Routes
+    app.use('/', homeRoutes);
+    app.use('/', submitRoutes);
 
-// Definer en grundlæggende GET-rute til rod-URL'en
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
-});
+    module.exports = app;
 
-// Fejlhåndtering middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
