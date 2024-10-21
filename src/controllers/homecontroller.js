f190822e4436475edd49f7cb83657a9a3303ee1b
amalieasborg <amalieasborg@gmail.com>
2024-10-21 15:57:05 +0200

    Get-metode til karakterer

--- src/controllers/homecontroller.js
+++ src/controllers/homecontroller.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+
 exports.getHomePage = (req, res) => {
-    res.send('Welcome to the Home Page!');
+    res.sendFile('C:/Users/Bruger/WebstormProjects/starwars/public/views/homepage.html');
 };
+
+//Array til at opbevare karakterer midlertidigt på serveren
+const characters=[];
+
+//GET /characters: Henter alle karakterer.
+app.get('/characters',(req,res)=>{
+    res.json(characters);
+});
